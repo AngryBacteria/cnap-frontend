e4613e4cb5fc7feae589b86781d8f8ba7aaded43
AngryBacteria <AngryBacteria@protonmail.com>
2023-03-08 21:21:19 +0100

    q-tabs with champion images

--- src/pages/League/LOLMatch.vue
+++ src/pages/League/LOLMatch.vue
@@ -2,65 +2,53 @@
   {{ url }}
 
   <div v-for="participant in data?.info.participants" :key="participant.puuid">
+    <img width="50" :src="rh.getChampionSquaredPortrait(participant.championId)">
     {{ participant.summonerName }} --
     {{ participant.kills }}/{{ participant.deaths }}/{{ participant.assists }} --
     {{ participant.championName }}
   </div>
 
-  <q-card>
-    <apexchart height="300" type="bar" :options="options" :series="series"></apexchart>
-  </q-card>
+  <q-tabs
+    v-model="participant"
+    class="text-teal"
+  >
+    <q-tab
+      v-for="participant in data?.info.participants" :key="participant"
+      :name="participant.championName"
+      :icon="`img:${rh.getChampionSquaredPortrait(participant.championId)}`"
+      :label="participant.summonerName"
+    />
+
+
+  </q-tabs>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
 import { useFetch } from '@vueuse/core'
 import { MatchDTO } from 'src/data/MatchInterfaces';
-import { computed } from 'vue';
-import {useQuasar} from 'quasar';
-import { getCssVar } from 'quasar'
+import {computed, ref} from 'vue';
+import RiotHelper from 'src/plugins/RiotHelper';
 
 const route = useRoute()
-const $q = useQuasar();
+const participant = ref('')
+const rh = RiotHelper.getInstance()
 
 const url = computed(() => {
   const matchId = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
   return `http://157.90.27.91:3000/api/v1/match/archive/id/${matchId}`
 })
 
-const options = computed(() => {
-  return {
-    theme: {
-      mode: $q.dark.mode ? 'dark' : 'light',
-    },
-    title: {
-      text: 'ApexArea',
-      align: 'left'
-    },
-    chart: {
-      background: $q.dark.mode ? getCssVar('dark') : '#ffffff',
-      height: 300,
-      type: 'bar'
-    },
-    markers: {
-      size: 4,
-      hover: {
-        sizeOffset: 6
-      }
-    },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-    }
-  }
-})
-
-const series = computed(() => {
-  return [{
-    name: 'series1',
-    data: [31, 40, -28, -51, 42, 109, 100],
-    color: ({ value }) => (value < 0) ? '#ff2576' : '#38c6f4'
-  }]
-})
-
 const { data } = await useFetch(url, { refetch: true }).get().json<MatchDTO>()
+
 </script>
+
+<style>
+.q-tab__label {
+  font-size: 1em;
+}
+.q-tab__icon {
+  width: 50px;
+  height: 50px;
+}
+</style>
